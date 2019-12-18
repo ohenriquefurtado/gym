0087d117e7e672892c55cb619d2c18abf4f7acd1
henriquefurtadob@gmail.com <henriquefurtadob@gmail.com>
2019-12-18 15:48:27 -0300

    configurações iniciais

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'henriquefurtadobarros',
+  password: '123456',
+  database: 'gym',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
